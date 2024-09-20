d2e4b9fc30ab083b26bf1f0932cc117a0f266332
Monir <mr.monirbd@gmail.com>
2024-09-20 12:44:24 +0600

    booksee365 mobile menu fix update

--- resources/views/templates/booksee365com/pages/cancel-membership.blade.php
+++ resources/views/templates/booksee365com/pages/cancel-membership.blade.php
@@ -1,14 +1,38 @@
-@extends('templates.booksee365com.layouts.main')
+@extends('templates.booksee365com.welcome.main')
 
 @section('styles-files')
-<link href="{{ asset ('booksee365com/theme_backup/css/subscription/style.css') }}" rel="stylesheet">
-<link href="{{ asset ('booksee365com/theme_backup/common/default.css') }}" rel="stylesheet">
+
+    <link href="{{ asset ('booksee365com/theme_backup/css/subscription/style.css') }}" rel="stylesheet">
+    <link href="{{ asset ('booksee365com/theme_backup/css/customer-product/style.css') }}" rel="stylesheet"> 
+    {{-- <link href="{{ asset ('booksee365com/theme_backup/common/default.css') }}" rel="stylesheet"> --}}
 @endsection
 
 @section('content')
+<style>
+    .member {
+  background-color: #fff;
+  padding-left: 270px;
+  padding-right: 24px;
+  padding-top: 20px;
+  padding-bottom: 80px;
+}
+.common label{
+color: #8f8989;
+}
+.common input{
+    color: #8f8989;
+    border: 1px solid rgba(77, 50, 50, 0.4);
+}
+.s-number p{
+    color: #8f8989;
+}
+.member-table td{
+    color: #8f8989;
+}
+</style>
 
 <section>
-    <div class="member pb-5">
+    <div class="member pb-5 mt-5">
         <div class="container-fluid background">
             <div class="row">
                 <div class="col-12">
