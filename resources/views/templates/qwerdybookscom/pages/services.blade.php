cf4a061ab701beb37eb30e68a223141dc1d7518a
Monir <mr.monirbd@gmail.com>
2024-10-07 10:56:38 +0600

    mentoring library data updated

--- resources/views/templates/qwerdybookscom/pages/services.blade.php
+++ resources/views/templates/qwerdybookscom/pages/services.blade.php
@@ -19,54 +19,60 @@
     .belong-overview {
         font-family: "Kanit", sans-serif !important;
     }
+
+    .hero__content {
+      padding-top: 50px !important;
+      padding-bottom: 80px;
+      position: relative;
+      z-index: 1;
+    }
+
+    .hero__content a {
+      display: inline-block;
+      max-width: none;
+      font-weight: 400;
+      font-size: 20px;
+      line-height: 1.5;
+      color: var(--white);
+      padding-bottom: 0px;
+      border-bottom: none;
+      position: relative;
+      overflow: visible;
+    }
    
 </style>
 <div id="smooth-wrapper">
     <div id="smooth-content">
       <main>
 
-        <!-- Hero area start -->
-        <section class="hero__about">
-          <div class="container g-0 line">
-            <span class="line-3"></span>
-            <div class="row">
-              <div class="col-xxl-12">
-                <div class="hero__about-content">
-                  
-                  @if ($pageName == 'terms_of_service')    
-                    <h1>Terms & Conditions</h1>
-                  @elseif ($pageName == 'privacy_policy') 
-                    <h1>Privacy Policy</h1>
-                  @else
-                    {{-- <h1>Subscription Policy</h1> --}}
-                  @endif                      
-
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section class="hero__about">
-          <div class="container g-0 line">
-            <span class="line-3"></span>
-            <div class="row">
-              <div class="col-xxl-12">
-                <div class="hero__about-content" style="padding-top: 50px">
-
-					        {!! $service !!}
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
         <!-- Hero area end -->
 
 
 
        
 
-      </main>
-  
+        <section class="hero__area ">
+          <div class="container">
+            <div class="row">
+              <div class="col-xxl-12">
+                <div class="hero__content animation__hero_one">
+                  @if ($pageName == 'terms_of_service')    
+                  <h1 class="pt-5 mt-5">Terms & Conditions</h1>
+                    @elseif ($pageName == 'privacy_policy') 
+                      <h1>Privacy Policy</h1>
+                    @else
+                      {{-- <h1>Subscription Policy</h1> --}}
+                    @endif                      
+
+                </div>
+                  <div class="hero__content animation__hero_one">
+                    {!! $service !!}
+                  </div>
+              </div>
+            </div>
+          </div>
+
+        </section>  
       
 
 
