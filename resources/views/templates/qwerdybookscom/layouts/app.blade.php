bf053c86480685e89c6a3f2ce0fea089adec1bbd
monir <mr.monirbd@gmail.com>
2024-11-21 03:53:30 +0000

    live update

--- resources/views/templates/qwerdybookscom/layouts/app.blade.php
+++ resources/views/templates/qwerdybookscom/layouts/app.blade.php
@@ -5,8 +5,16 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="Axtra HTML5 Template">
+  <meta name="description" content="">
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-3S0401Y9MP"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
+    gtag('config', 'G-3S0401Y9MP');
+  </script>
   <link rel="icon" type="image/x-icon" href="{{asset('qwerdybookscom/') }}//assets/imgs/logo/favicon.png">
 
   <title>Qwerdybooks</title>
