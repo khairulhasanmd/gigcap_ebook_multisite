56a607d1d93a50143b536b2ad57d5b89c4dffe38
Monir <mr.monirbd@gmail.com>
2024-11-25 14:44:07 +0600

    g-tag added to digiventro

--- resources/views/templates/mentoringlibrarycom/welcome/master.blade.php
+++ resources/views/templates/mentoringlibrarycom/welcome/master.blade.php
@@ -10,6 +10,16 @@
                 display: none;
             }
         </style>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-QJKNNKJYWS"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-QJKNNKJYWS');
+        </script>
+
     </head>
 
     <body>
