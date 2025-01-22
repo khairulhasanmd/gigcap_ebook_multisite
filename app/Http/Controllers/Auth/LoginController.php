7ebab9e51f0c2119205e22e4c5c0b1d72c96e107
Monir <mr.monirbd@gmail.com>
2025-01-22 23:06:37 +0600

    adminpannel updated

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -69,6 +69,53 @@ class LoginController extends Controller
 
     }
 
+    // protected function redirectTo()
+    // {  
+    //     $username = request()->get('email');
+    //     $password = request()->get('password');
+        
+    //     if (auth()->user()->is_admin) {
+    //         return '/administrator/dashboard';
+    //     } 
+    //     else {
+    //         $users = User::where('email', '=', auth()->user()->email)->get();
+    //         if(!$users) {
+    //             dd($request);
+    //             $userLogin = $this->cmp->authenticateUser($request, $username, $password);
+    //             if ($userLogin->status == 'success') {
+    //                 if ($this->concept->concept_type == "coding") {
+    //                     return route('courses');
+    //                 } else  if ($this->concept->concept_type == "bbt") { 
+    //                     return route('dashboard');
+    //                 } else if ($this->concept->concept_type == "gaming") {
+    //                     return route('dashboard');
+    //                 } else {
+    //                     return redirect()->route('login')->with('error','Something went wrong');
+    //                 }
+    //             } else {
+    //                 return redirect()->intended('login')
+    //                 ->with('error','Email-Address And Password Are Wrong.');
+    //             }
+    //         } else {
+    //             foreach ($users as $user) {
+    //                 if($user->domain == $this->currentDomain) {
+    //                     if ($this->concept->concept_type == "coding") {
+    //                         return route('courses');
+    //                     } else  if ($this->concept->concept_type == "bbt") { 
+    //                         return route('dashboard');
+    //                     } else if ($this->concept->concept_type == "gaming") {
+    //                         return route('dashboard');
+    //                     } else {
+    //                         return redirect()->route('login')->with('error','Something went wrong');
+    //                     }
+    //                 } else {
+    //                     return redirect()->route('login')->with('error','Something went wrong');
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+
     public function showLoginForm() {
         return view('templates.'.$this->concept->template.'.auth.login');
 
@@ -90,70 +137,56 @@ class LoginController extends Controller
         return redirect()->route('login');
     }
 
-    public function login(Request $request)
+    protected function login()
 {  
-    $username = $request->email;
-    $password = $request->password;
-
-    $request->setLocale(app()->getLocale());
-    $userLogin = $this->cmp->authenticateUser($request, $username, $password);
-
-    $userLoginArr = [
-        'email' => $request->email,
-        'password' => $request->password,
-        'enabled' => 1
-    ];
-    //blocking other users from cross site login
-    if(!($request->email == 'admin@52north.co' || $request->email == 'superadmin@52north.co')){
-        $userLoginArr['id'] = $this->concept->id;
-    }
-    if ($this->hasTooManyLoginAttempts($request)) {
-        $this->fireLockoutEvent($request);
-        return $this->sendLockoutResponse($request);
-    }
-    if ($userLogin->status == 'success') {
-        $user = $this->isWebUserId($userLogin->data, $username, $request->password);
-            // dd($request->password);
+    $username = request()->get('email');
+    $password = request()->get('password');
+    if (Auth::attempt(['email' => $username, 'password' => $password, 'domain_name' => $this->currentDomain])) { 
 
 
-        Auth::loginUsingId($user->id);
-        return $this->authenticated($request);
-    }else if(Auth::attempt($userLoginArr)) {//conventional login process for admin, superadmin or local users
-        return $this->authenticated($request);
-    }
-    // If the login attempt was unsuccessful we will increment the number of attempts
-    // to login and redirect the user back to the login form. Of course, when this
-    // user surpasses their maximum number of attempts they will get locked out.
-    $this->incrementLoginAttempts($request);
+        if(Auth::user()->is_admin == 1){
+            return redirect()->route('concepts');
+        }else{
 
-    return $this->sendFailedLoginResponse($request);
-}
-
-protected function authenticated(Request $request)
-{
-    $user = Auth::user();
-    // dd($user);
-    $this->clearLoginAttempts($request);
-    if ( isset($user->is_admin) && ($user->is_admin == 1 )) {// do your magic here
-        // return redirect('concepts');
-        return redirect()->route('concepts');
-
+            return redirect()->route('products');
+        
     }
 
-    return redirect()->route('products');
+    }else if (Auth::attempt(['email' => $username, 'password' => $password, 'is_admin' => 1])) {
 
+       
+
+    // Third attempt for other cases
+    } else if ($this->cmp->authenticateUser(request(), $username, $password)->status == 'success') {
+
+        $user = $this->cmp->authenticateUser(request(), $username, $password);
+        // dd($password);
+        $this->isWebUserId($user->data, $username, $password);
+        // dd($this->currentDomain);
+            
+        if (Auth::attempt(['email' => $username, 'password' => $password, 'domain_name' => $this->currentDomain])) {
+            return redirect()->route('products');
+        }
+        // dd('testing');
+
+    // Third attempt for other cases
+    } 
+     else {
+        // dd('wrong');
+        // return redirect()->intended('login')
+        //     ->withErrors('error', 'Email-Address And Password Are Wrong.');
+        return redirect()->back()->withErrors(['error' => 'Email-Address And Password Are Wrong']);
+    }
 }
     
 
 // return redirect()->back()->withErrors(['error' => 'New password is not repeated twice']);
 
 
-public function isWebUserId($data, $username, $password){
-    $existingUser = User::where('customer_id', $data->customer_id)->first();
-// dd($existingUser);
-    $existingUserByEmail = User::where('email', $data->username)->first();
-    if ($existingUser) {
-        $userInfo = $this->cmp->getCustomer($data->customer_id);
+    public function isWebUserId($data, $username, $password) {
+        $existingUser = User::where('customer_id', $data->customer_id)->where('domain_name', '=', $this->currentDomain)->first();
+        if (!$existingUser) {
+            $userInfo = $this->cmp->getCustomer($data->customer_id);
             $subData = $this->cmp->getSubscriptionDetailByCustomerId($data->customer_id);
             // dd($data->id);
 
@@ -175,37 +208,35 @@ public function isWebUserId($data, $username, $password){
             $user->webshop_user_id  = $data->webshop_id;
             $user->site_user_id = $data->id;
             $user->domain_name = $this->currentDomain;
-        $existingUserByEmail->update();
 
-        return $existingUserByEmail;
-    }else{
-        $custData = [
-            'customerId' => $data->customer_id
-        ];
-        $userInfo = $this->crmApi->get(CrmApi::$routes['customer_get'], $custData);
+            $user->save();
+        } else {
+            $existingUserEmail = User::where('email', $username)->where('domain_name', '=', $this->currentDomain)->first();
+            if ($existingUserEmail) {
+                $userInfo = $this->cmp->getCustomer($data->customer_id);
+                $subData = $this->cmp->getSubscriptionDetailByCustomerId($data->customer_id);
 
-        $subData = $this->crmApi->get(CrmApi::$routes['customer_subscriptions_get'], $custData);
-        $subscription = $subData->getItems()[0];
+                $subscription = $subData->data[0];
+                
+                $existingUserEmail->email = $username;
+                $existingUserEmail->name = $userInfo->data->first_name." ".$userInfo->data->last_name;
+                $existingUserEmail->email_canonical = strtolower($username);
+                $existingUserEmail->username = $userInfo->data->first_name."-".$userInfo->data->last_name;
+                $existingUserEmail->username_canonical = strtolower($userInfo->data->first_name."-".$userInfo->data->last_name);
+                $existingUserEmail->plain_password = $password;
+                $existingUserEmail->role = 'ROLE_SUBSCRIBER';
+                $existingUserEmail->enabled = 1;
+                $encryptedPassword = Hash::make($password);
+                $existingUserEmail->password = $encryptedPassword;
+                $existingUserEmail->subscription_id = $subscription->id;
+                $existingUserEmail->customer_id = $data->customer_id;
+                $existingUserEmail->domain = $this->currentDomain;
 
-        $newUser = new User();
-        $newUser->concept_id = $this->concept->id;
-        $newUser->web_user_id = $data->id;
-        $newUser->name = $userInfo->getFirstName().' '.$userInfo->getLastName();
-        $newUser->created_at = $userInfo->getCreatedAt();
-        // $newUser->email = $data->getUsername();
-        // $newUser->username_canonical = $userInfo->getFirstName().' '.$userInfo->getLastName();
-        $newUser->username = $userInfo->getEmail();
-        $newUser->email = $username;//$userInfo->getEmail();
-        // $newUser->email_canonical = $data->getEmail();
-        $newUser->subscription_id = $subscription->getId();
-        $newUser->level = 'ROLE_SUBSCRIBER';
-        $newUser->customer_id = $userInfo->getId();
-        $newUser->plain_password = $password;
-        $newUser->password = password_hash($newUser->plain_password, PASSWORD_DEFAULT);
-        $newUser->country_code = $userInfo->getCountryCode();
-        $newUser->save();
+                return redirect()->route('products');
 
-        return redirect()->route('products');
+            }
+
+        }
+
     }
-}
 }
